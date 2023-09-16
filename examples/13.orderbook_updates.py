0331b5e1d6c0fcb081e675db0a6f94a8fc2372e1
Nida Karim Ali <nidakarimali@Nidas-MacBook-Pro.local>
2023-09-16 15:04:46 +0500

    updated example

--- examples/13.orderbook_updates.py
+++ examples/13.orderbook_updates.py
@@ -1,13 +1,10 @@
-"""
-When ever the state of orderbook changes, an event is emitted by exchange.
-In this code example we open a socket connection and listen to orderbook update event
-"""
+###
+# Places a market order on exchange and listens to emitted events
+##
 import sys, os
-
 sys.path.append(os.getcwd() + "/src/")
 import time
 from config import TEST_ACCT_KEY, TEST_NETWORK
-import asyncio
 from bluefin_v2_client import (
     BluefinClient,
     Networks,
@@ -15,39 +12,16 @@ from bluefin_v2_client import (
     SOCKET_EVENTS,
     ORDER_SIDE,
     ORDER_TYPE,
+    ORDER_STATUS,
     OrderSignatureRequest,
 )
-
+import asyncio
 
 event_received = False
 
 
-async def place_limit_order(client: BluefinClient):
-    # default leverage of account is set to 3 on Bluefin
-    user_leverage = await client.get_user_leverage(MARKET_SYMBOLS.ETH)
-
-    # creates a LIMIT order to be signed
-    signature_request = OrderSignatureRequest(
-        symbol=MARKET_SYMBOLS.ETH,  # market symbol
-        price=1300,  # price at which you want to place order
-        quantity=0.01,  # quantity
-        side=ORDER_SIDE.SELL,
-        orderType=ORDER_TYPE.LIMIT,
-        leverage=user_leverage,
-    )
-    # create signed order
-    signed_order = client.create_signed_order(signature_request)
-
-    print("Placing a limit order")
-    # place signed order on orderbook
-    resp = await client.post_signed_order(signed_order)
-
-    # returned order with PENDING state
-    print(resp)
-    return
-
-
 async def main():
+
     client = BluefinClient(True, Networks[TEST_NETWORK], TEST_ACCT_KEY)
     await client.init(True)
 
@@ -56,36 +30,69 @@ async def main():
         print("Event data:", event)
         event_received = True
 
-    # must open socket before subscribing
-    print("Making socket connection to Bluefin exchange")
+    async def connection_callback():
+        # This callback will be invoked as soon as the socket connection is established
+        # subscribe to global event updates for BTC market
+        status = await client.socket.subscribe_global_updates_by_symbol(MARKET_SYMBOLS.ETH)
+        print("Subscribed to global ETH events: {}".format(status))
+
+        # triggered when orderbook updates are received
+        print("Listening to orderbook updates")
+        await client.socket.listen(SOCKET_EVENTS.ORDERBOOK_UPDATE.value, callback)
+
+    async def disconnection_callback():
+        print("Sockets disconnected, performing actions...")
+        resp = await client.cancel_all_orders(MARKET_SYMBOLS.ETH, [ORDER_STATUS.OPEN, ORDER_STATUS.PARTIAL_FILLED])
+        print(resp)
+
+
+# must specify connection_callback before opening the sockets below
+    await client.socket.listen("connect", connection_callback)
+    await client.socket.listen("disconnect", disconnection_callback)
+
+    print("Making socket connection to firefly exchange")
     await client.socket.open()
 
-    # subscribe to global event updates for ETH market
-    await client.socket.subscribe_global_updates_by_symbol(MARKET_SYMBOLS.ETH)
-    print("Subscribed to ETH Market events")
+    ######## Placing an Order ########
 
-    print("Listening to ETH Orderbook update event")
-    await client.socket.listen(SOCKET_EVENTS.ORDERBOOK_UPDATE.value, callback)
+    # default leverage of account is set to 3 on firefly
+    user_leverage = await client.get_user_leverage(MARKET_SYMBOLS.ETH)
 
-    await place_limit_order(client)
+    # creates a MARKET order to be signed
+    signature_request = OrderSignatureRequest(
+        symbol=MARKET_SYMBOLS.ETH,
+        leverage=user_leverage,
+        price=1300,
+        quantity=0.5,
+        side=ORDER_SIDE.BUY,
+        orderType=ORDER_TYPE.LIMIT
+    )
 
-    timeout = 30
+    # create signed order
+    signed_order = client.create_signed_order(signature_request)
+
+    print("Placing a market order")
+    # place signed order on orderbook
+    resp = await client.post_signed_order(signed_order)
+    print(resp)
+    ###### Closing socket connections after 30 seconds #####
+    timeout = 10
     end_time = time.time() + timeout
     while not event_received and time.time() < end_time:
         time.sleep(1)
-    status = await client.socket.unsubscribe_global_updates_by_symbol(
-        MARKET_SYMBOLS.ETH
-    )
-    print("Unsubscribed from orderbook update events for ETH Market: {}".format(status))
 
-    # close socket connection
+    # # close socket connection
     print("Closing sockets!")
     await client.socket.close()
 
-    await client.close_connections()
-
 
 if __name__ == "__main__":
+    ### make sure keep the loop initialization same 
+    # as below to ensure closing the script after receiving 
+    # completion of each callback on socket events ###  
     loop = asyncio.new_event_loop()
-    loop.run_until_complete(main())
+    loop.create_task(main())
+    pending = asyncio.all_tasks(loop=loop)
+    group = asyncio.gather(*pending)
+    loop.run_until_complete(group)
     loop.close()
