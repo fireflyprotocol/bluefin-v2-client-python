afe5331a958363ca9684dbc9c10df1ffc3351085
Nida Karim Ali <110534850+NidaKarimAli@users.noreply.github.com>
2023-09-16 14:22:32 +0500

    [EXC-1685] add support for growth apis to sui python client (#18)
    * add growth routes to python client
    
    * updated version
    
    ---------
    
    Co-authored-by: Nida Karim Ali <nidakarimali@Nidas-MacBook-Pro.local>

--- src/bluefin_v2_client/interfaces.py
+++ src/bluefin_v2_client/interfaces.py
@@ -240,3 +240,181 @@ class FailedCountDownResetResponse(TypedDict):
 class PostTimerResponse(TypedDict):
     acceptedToReset: List[str]
     failedReset: List[FailedCountDownResetResponse]
+
+
+class GenerateReferralCodeRequest(TypedDict):
+  referralCode: str
+  campaignId: int
+
+class GenerateReferralCodeResponse(TypedDict):
+  referralAddress: str
+  referralCode: str
+  message: str
+
+class LinkReferredUserRequest(TypedDict):
+  referralCode: str
+  campaignId: int
+
+class LinkReferredUserResponse(TypedDict):
+  referralCode: str
+  refereeAddress: str
+  campaignId: int
+  message: str
+
+class GetReferrerInfoResponse(TypedDict):
+  isReferee: bool
+
+
+class CampaignDetailsConfig(TypedDict):
+  cashShare: str
+  tokenShare: str
+  shareOfFees: str
+  refereeDiscount: str
+
+class GetCampaignDetailsResponse(TypedDict):
+  id: str
+  campaignName: str
+  parentCampaignName: str
+  startDate: str
+  endDate: str
+  isActive: bool
+  epochDurationSeconds: int
+  config: CampaignDetailsConfig
+  createdAt: str
+  updatedAt: str
+
+class GetCampaignRewardsResponse(TypedDict):
+  campaignName: str
+  campaignConfig: CampaignDetailsConfig
+  cashReward: str
+  tokenReward: str
+
+class GetAffiliatePayoutsResponse(TypedDict):
+  epStartDate: str
+  epEndDate: str
+  isActive: bool
+  totalReferralFees: str
+  cashReward: str
+  tokenReward: str
+  epochNumber: str
+
+class GetAffiliateRefereeDetailsRequest(TypedDict):
+  campaignId: int
+  pageNumber: int
+  pageSize: int
+
+class AffiliateRefereeDetailsData(TypedDict):
+  userAddress: str
+  lastTraded: str
+  dateJoined: str
+  feesPaid: str
+
+class GetAffiliateRefereeDetailsResponse(TypedDict):
+  data: List[AffiliateRefereeDetailsData]
+  nextCursor: int
+  isMoreDataAvailable: bool
+
+
+class GetAffiliateRefereeCountResponse(TypedDict):
+  referralCode: str
+  referralCount: int
+
+
+class GetUserRewardsHistoryRequest(TypedDict):
+  pageSize: int
+  cursor: int
+
+class UserRewardsHistoryData(TypedDict):
+  programName: str
+  parentProgramName: str
+  startDate: str
+  endDate: str
+  cashReward: str
+  tokenReward: str
+  isActive: bool
+  cursor: str
+  epochNumber: str
+
+class GetUserRewardsHistoryResponse(TypedDict):
+  data: List[UserRewardsHistoryData]
+  nextCursor: int
+  isMoreDataAvailable: bool
+
+class RewardsSummaryData(TypedDict):
+  campaignName: str
+  totalCashReward: str
+  totalTokenReward: str
+
+class GetUserRewardsSummaryResponse(TypedDict):
+  totalTokenReward: str
+  totalCashReward: str
+  campaignData: List[RewardsSummaryData]
+
+class GetTradeAndEarnRewardsOverviewResponse(TypedDict):
+  totalHistoricalRewards: str
+  totalActiveRewards: str
+  totalFeePaid: str
+  latestEpochNumber: str
+  latestEpochStart: int
+  latestEpochEnd: int
+  latestEpochTotalFee: str
+  latestEpochTotalRewards: str
+
+
+class GetTradeAndEarnRewardsDetailRequest(TypedDict):
+  campaignId: int
+  pageSize: int
+  cursor: int
+
+class TradeAndEarnRewardsDetailData(TypedDict):
+  tradingRewards: str
+  feePaid: str
+  cursor: str
+  id: int
+  isActive: bool
+  startDate: str
+  endDate: str
+  epochNumber: str
+
+class GetTradeAndEarnRewardsDetailResponse(TypedDict):
+  data: List[TradeAndEarnRewardsDetailData]
+  nextCursor: str
+  isMoreDataAvailable: bool
+
+class GetTotalHistoricalTradingRewardsResponse(TypedDict):
+  totalTokenRewards: str
+
+class GetMakerRewardsSummaryResponse(TypedDict):
+  latestEpochStart: int
+  latestEpochEnd: int
+  latestEpochNumber: str
+  latestEpochTotalRewards: str
+  totalActiveRewards: str
+  totalHistoricalRewards: str
+
+class GetMakerRewardDetailsRequest(TypedDict):
+  symbol: str
+  pageSize: int
+  cursor: int
+
+
+class MakerRewardDetailsData(TypedDict):
+  latestEpochNumber: str
+  status: str
+  makerVolume: str
+  volumePercentage: str
+  rewardPoolPercentage: str
+  makerRewards: str
+  startDate: str
+  endDate: str
+  cursor: str
+  liquidityScore: str # come if symbol provided in request
+  uptimePercentage: str # come if symbol provided in request
+
+class GetMakerRewardDetailsResponse(TypedDict):
+  data: List[MakerRewardDetailsData]
+  nextCursor: int
+  isMoreDataAvailable: bool
+
+class GetUserWhiteListStatusForMarkeMakerResponse(TypedDict):
+  isWhitelist: bool
