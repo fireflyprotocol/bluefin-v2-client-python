2b3a60f6903db880314be982e08e86ca9a11a665
burhanuddin03 <burhanuddinmshabbir@gmail.com>
2024-03-13 16:53:16 +0500

    fetch default leverage from exchange info route

--- src/bluefin_v2_client/client.py
+++ src/bluefin_v2_client/client.py
@@ -1087,8 +1087,10 @@ class BluefinClient:
         for i in account_data_by_market:
             if symbol.value == i["symbol"]:
                 return from1e18(int(i["selectedLeverage"]))
+            
+        exchange_info_by_market = await self.get_exchange_info(symbol.value)
         # todo fetch from exchange info route
-        return DEAULT_EXCHANGE_LEVERAGE
+        return exchange_info_by_market.defaultLeverage
 
     async def get_cancel_on_disconnect_timer(
         self, params: GetCancelOnDisconnectTimerRequest = None
