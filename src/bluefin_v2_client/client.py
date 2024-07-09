714319016229c7d8172d434687b90e1f2749d8c0
Kulsoom Malik <kulsoommalik@Kulsooms-MacBook-Pro-2.local>
2024-07-09 23:31:40 +0500

    Added sui clock time

--- src/bluefin_v2_client/client.py
+++ src/bluefin_v2_client/client.py
@@ -5,7 +5,7 @@ from .api_service import APIService
 from .contracts import Contracts
 from .order_signer import OrderSigner
 from .onboarding_signer import OnboardingSigner
-from .constants import TIME, SERVICE_URLS
+from .constants import SUI_CLOCK_OBJECT_ID, TIME, SERVICE_URLS
 from .sockets_lib import Sockets
 from .websocket_client import WebsocketClient
 from .signer import Signer
@@ -514,6 +514,7 @@ class BluefinClient:
         # implies user has an open position on-chain, perform on-chain leverage update
         if user_position != {}:
             callArgs = []
+            callArgs.append(SUI_CLOCK_OBJECT_ID)
             callArgs.append(self.contracts.get_perpetual_id(symbol))
             callArgs.append(self.contracts.get_bank_id())
             callArgs.append(self.contracts.get_sub_account_id())
@@ -599,6 +600,7 @@ class BluefinClient:
             raise (Exception(f"User has no open position on market: {symbol}"))
 
         callArgs = []
+        callArgs.append(SUI_CLOCK_OBJECT_ID)
         callArgs.append(self.contracts.get_perpetual_id(symbol))
         callArgs.append(self.contracts.get_bank_id())
 
