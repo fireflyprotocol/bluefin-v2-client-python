3352c5daa35e5d69a6f340f80f30e647152f9248
Abdullah Abro <kamario.abdullah@gmail.com>
2025-02-16 05:03:03 +0500

    [BFS-808] Update get vault balance method as per contracts changes (#76)

--- src/sui_utils/utilities.py
+++ src/sui_utils/utilities.py
@@ -17,6 +17,7 @@ BASE_1E18 = 1000000000000000000
 BASE_1E6 = 1000000  # 1e6 for USDC token
 BASE_1E9 = 1000000000
 SUI_STRING_OBJECT_TYPE = "0x1::string::String"
+SUI_CUSTOM_OBJECT_TYPE = "0x1::type_name::TypeName"
 
 
 def getsha256Hash(callArgs: list) -> str:
